da1d2f56808bd413923268cb12b663a264f7764c
Miguel Cedillo <miacedil@ucsc.edu>
2021-04-16 05:09:23 -0700

    game over screen

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -55,6 +55,13 @@ class Play extends Phaser.Scene{
             fixedWidth: 100
         }
         this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
+        //60 - second play clock
+        scoreConfig.fixedWidth = 0;
+        this.clock = this.time.delayedCall(1000, ()=>{
+            this.add.rectangle(0,0,game.config.width, game.config.height, 'white',  0.5);
+            this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+        }, null, this);
     }
 
     update(){
@@ -101,5 +108,8 @@ class Play extends Phaser.Scene{
             ship.alpha = 1; //make ship visible again
             boom.destroy(); // remove explosion sprite
         });
+        //score add and repaint
+        this.p1Score += ship.points;
+        this.scoreLeft.text = this.p1Score;
     }
 }
