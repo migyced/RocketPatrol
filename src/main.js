87e9f973dda969c8f7106a836d8571b03ce6eedd
Miguel Cedillo <miacedil@ucsc.edu>
2021-04-16 02:32:31 -0700

    rocket class

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ let config = {
 }
 
 let game = new Phaser.Game(config);
+let keyF, keyR, keyLEFT, keyRIGHT;
 
 //set UI sizes
 let borderUISize = game.config.height / 15;
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -20,6 +20,13 @@ class Play extends Phaser.Scene{
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
+        //add rocket (p1)
+        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0,0);
+        //define keys
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
 
     update(){
