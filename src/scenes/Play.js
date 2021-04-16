a429b0d05d8bec21a1e07a10d5ecfcef791459ba
Miguel Cedillo <miacedil@ucsc.edu>
2021-04-16 05:47:43 -0700

    menu screen and audio

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,6 +10,7 @@ class Play extends Phaser.Scene{
         this.load.image('starfield', './assets/starfield.png');
         //load spritesheet
         this.load.spritesheet('explosion','./assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});// sets explosion order from frame 0 to frame 9 & defines the dimensions of a frame
+        
     }
 
     create(){
@@ -57,19 +58,32 @@ class Play extends Phaser.Scene{
         this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
         //60 - second play clock
         scoreConfig.fixedWidth = 0;
-        this.clock = this.time.delayedCall(1000, ()=>{
+        //game over flag
+        this.gameOver = false;
+
+        this.clock = this.time.delayedCall(game.settings.gameTimer, ()=>{
             this.add.rectangle(0,0,game.config.width *2, game.config.height*2, 'black',  0.5);
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
-            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or <- for Menu', scoreConfig).setOrigin(0.5);
+            this.gameOver = true;
         }, null, this);
     }
 
     update(){
+        //check key input for restart
+        if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)){
+            this.scene.restart();
+        }
+        if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)){
+            this.scene.start("menuScene");
+        }
         this.starfield.tilePositionX -=5;
-        this.p1Rocket.update();
-        this.ship01.update();
-        this.ship02.update();
-        this.ship03.update();
+        if(!this.gameOver){
+            this.p1Rocket.update();
+            this.ship01.update();
+            this.ship02.update();
+            this.ship03.update();
+        }
         //check collisions
         if(this.checkCollision(this.p1Rocket, this.ship03)){
             this.p1Rocket.reset();
@@ -111,5 +125,6 @@ class Play extends Phaser.Scene{
         //score add and repaint
         this.p1Score += ship.points;
         this.scoreLeft.text = this.p1Score;
+        this.sound.play('sfx_explosion');
     }
 }
