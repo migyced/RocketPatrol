270bdae32047966b4f525351812bf9f170b4ce7f
Miguel Cedillo <miacedil@ucsc.edu>
2021-04-16 01:16:57 -0700

    .

--- src/main.js
+++ src/main.js
@@ -7,3 +7,7 @@ let config = {
 }
 
 let game = new Phaser.Game(config);
+
+//set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize /3;
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,6 +4,8 @@ class Play extends Phaser.Scene{
     }
 
     create(){
-        this.add.text(20,20, "Rocket Patrol Play");
+        // green UI background
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize*2, 0x00FF00).setOrigin(0, 0);
+        
     }
 }
