cadda1ba5c29f848d429670eb3af3690a7646c3a
Miguel Cedillo <miacedil@ucsc.edu>
2021-04-16 05:48:45 -0700

    .

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,9 +5,9 @@ class Menu extends Phaser.Scene{
 
     preload(){
         //load audio
-        this.preload.audio('sfx_select', './assets/blip_select12.wav');
-        this.preload.audio('sfx_explosion', './assets/explosion38.wav');
-        this.preload.audio('sfx_rocket', './assets/rocket_shot.wav');
+        this.load.audio('sfx_select', './assets/blip_select12.wav');
+        this.load.audio('sfx_explosion', './assets/explosion38.wav');
+        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
     }
 
     create(){
