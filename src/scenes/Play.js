42d79acb24bf1ad5c6648a647427c8409de03a44
Miguel Cedillo <miacedil@ucsc.edu>
2021-04-16 02:06:30 -0700

    moving background

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -13,10 +13,16 @@ class Play extends Phaser.Scene{
     create(){
         // green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize*2, 0xFFCCCC).setOrigin(0, 0);
+        //place tiel sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
         //white borders
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
+    }
+
+    update(){
+        this.starfield.tilePositionX -=5;
     }
 }
