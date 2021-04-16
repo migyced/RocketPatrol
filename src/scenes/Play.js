0c0bf9bc63929363d3d75a6da2bdbb639f920bac
Miguel Cedillo <miacedil@ucsc.edu>
2021-04-16 02:07:52 -0700

    .

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,10 +11,10 @@ class Play extends Phaser.Scene{
     }
 
     create(){
-        // green UI background
-        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize*2, 0xFFCCCC).setOrigin(0, 0);
         //place tiel sprite
         this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+        // green UI background
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize*2, 0xFFCCCC).setOrigin(0, 0);
         //white borders
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
