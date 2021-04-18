918af97569a82a9a177b5dedcf25090f3cb4ef8d
Miguel Cedillo <miacedil@ucsc.edu>
2021-04-18 03:35:41 -0700

    .

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -77,7 +77,7 @@ class Play extends Phaser.Scene{
         if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)){
             this.scene.start('menuScene');
         }
-        this.starfield.tilePositionX -=5;
+        this.starfield.tilePositionX -=3;
         if(!this.gameOver){
             this.p1Rocket.update();
             this.ship01.update();
