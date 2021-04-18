3845fa328f9e5f148c67b586b625ed9648d8c4f2
Miguel Cedillo <82360408+migyced@users.noreply.github.com>
2021-04-18 05:45:48 -0700

    Update Play.js

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -17,7 +17,7 @@ class Play extends Phaser.Scene{
         //place tiel sprite
         this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
         // green UI background
-        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize*2, 0x000000, 0.2).setOrigin(0, 0);
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize*2, 0xFFCCCC).setOrigin(0, 0);
         //white borders
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
@@ -77,7 +77,7 @@ class Play extends Phaser.Scene{
         if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)){
             this.scene.start('menuScene');
         }
-        this.starfield.tilePositionX -=2;
+        this.starfield.tilePositionX -=5;
         if(!this.gameOver){
             this.p1Rocket.update();
             this.ship01.update();
