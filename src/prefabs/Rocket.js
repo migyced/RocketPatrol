c1e28f60519fc90ea801ffbc73116eb3b4586eed
Miguel Cedillo <miacedil@ucsc.edu>
2021-04-19 07:12:53 -0700

    .

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,7 +1,7 @@
 //Rocket prefab
 class Rocket extends Phaser.GameObjects.Sprite{
-    constructor(scene, x, y, texture, frame){
-        super(scene, x, y, texture, frame);
+    constructor(scene, x, y, texture, frame, player){
+        super(scene, x, y, texture, frame, player);
 
         //add object to existing scene
         scene.add.existing(this); //add to existing, displayList, updateList
@@ -11,18 +11,34 @@ class Rocket extends Phaser.GameObjects.Sprite{
     } 
     update(){
         //left/right movement
-        if(!this.isFiring){
-            if(keyLEFT.isDown && this.x >= borderUISize + this.width){
-                this.x -= this.moveSpeed;
-            }else if(keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width){
-                this.x += this.moveSpeed;
+        if(this.player == 1){
+            if(!this.isFiring){
+                if(keyLEFT.isDown && this.x >= borderUISize + this.width){
+                    this.x -= this.moveSpeed;
+                }else if(keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width){
+                    this.x += this.moveSpeed;
+                }
+            }
+            //fire button
+            if(Phaser.Input.Keyboard.JustDown(keyUP)){
+                this.isFiring = true;
+                this.sfxRocket.play(); //play sfx
+        }
+        }else if(this.player == 2){
+            if(!this.isFiring){
+                if(keyA.isDown && this.x >= borderUISize + this.width){
+                    this.x -= this.moveSpeed;
+                }else if(keyD.isDown && this.x <= game.config.width - borderUISize - this.width){
+                    this.x += this.moveSpeed;
+                }
+            }//fire button
+            if(Phaser.Input.Keyboard.JustDown(keyW)){
+                this.isFiring = true;
+                this.sfxRocket.play(); //play sfx
             }
         }
-        //fire button
-        if(Phaser.Input.Keyboard.JustDown(keyUP)){
-            this.isFiring = true;
-            this.sfxRocket.play(); //play sfx
-        }
+        
+        
         //if fired, move up
         if(this.isFiring && this.y >= borderUISize*3 + borderPadding){
             this.y -= this.moveSpeed;
