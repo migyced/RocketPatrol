8d49ae97b73393b53e6e51a2a59f154d223ee93d
Miguel Cedillo <miacedil@ucsc.edu>
2021-04-19 09:36:27 -0700

    .

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
+/*----------------------------------------
+Name: Miguel Cedillo
+Class: CMPM 120
+Project Title: Rocket Patrol Mods aka Aqua Patrol
+Project Length: 15 hours
 //
+Point Breakdown
+ - Redesign the game's artwork,UI, & Sound(60pts): I changed the theme to be aquatic(fish vs boats)
+ - Create and implement a new weapon(20): The octopus is the new weapon, it is only used in the Novice level because
+    it is easier to hit the boats. When fired, the octopus will randomly move left and right as it travels up.
+ - Create a new spaceship(boat) that's smaller, moves faster, and is worth more points(20): I added the submarine to 
+    the game, it is worth 50 points and is partially submerged.
+----------------------------------------*/
 let config = {
     type: Phaser.AUTO,
     width: 640,
