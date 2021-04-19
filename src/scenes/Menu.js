15c72792b273261e8e52ed73262584f6c1f61a1d
Miguel Cedillo <miacedil@ucsc.edu>
2021-04-19 04:11:40 -0700

    .

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -29,7 +29,7 @@ class Menu extends Phaser.Scene{
         this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'AQUA PATROL', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height/2, 'Use <- -> arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
         menuConfig.backgroundColor = '#00FF00';
-        menuConfig.config = '#000';
+        menuConfig.config = '#1A7DFF';
         this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press <- for Novice or -> for Expert', menuConfig).setOrigin(0.5);
         //define keys
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
